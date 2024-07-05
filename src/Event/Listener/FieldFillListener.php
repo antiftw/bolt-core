a31d174051e75a03133c4626c27ec54b224f4cae
antiftw <atjanssen@hotmail.nl>
2024-07-05 20:56:23 +0200

    event listeners refactored to use attributes for symf 7 bump

--- src/Event/Listener/FieldFillListener.php
+++ src/Event/Listener/FieldFillListener.php
@@ -15,7 +15,7 @@ use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
 use Twig\Markup;
 
-#[AsDoctrineListener(event: Events::preUpdate, entity: FieldTranslation::class)]
+#[AsDoctrineListener(event: Events::preUpdate)]
 readonly class FieldFillListener
 {
     public function __construct(private Sanitiser $sanitiser) {}
