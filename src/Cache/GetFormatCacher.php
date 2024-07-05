9538a131fe38b3ca345fff91faf862787d9a1ffd
antiftw <atjanssen@hotmail.nl>
2024-07-05 22:09:22 +0200

    di into cache class since it was causing errors

--- src/Cache/GetFormatCacher.php
+++ src/Cache/GetFormatCacher.php
@@ -2,12 +2,30 @@
 
 namespace Bolt\Cache;
 
+use Bolt\Canonical;
+use Bolt\Configuration\Config;
 use Bolt\Entity\Content;
+use Bolt\Twig\LocaleExtension;
 use Bolt\Utils\ContentHelper;
+use Symfony\Component\HttpFoundation\RequestStack;
 
+use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 class GetFormatCacher extends ContentHelper implements CachingInterface
 {
     use CachingTrait;
+
+    public function __construct(
+        RequestStack $requestStack,
+        private readonly Canonical $canonical,
+        private readonly Config $config,
+        private readonly LocaleExtension $localeExtension,
+        private readonly Stopwatch $stopwatch,
+        private readonly TagAwareCacheInterface $cache,
+    ) {
+        parent::__construct($requestStack, $canonical, $config, $localeExtension);
+    }
 
     public const string CACHE_CONFIG_KEY = 'formatter';
 
